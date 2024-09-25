ef7c3edf8bae2641a279adb97f555533f8b78c2c
fabr404 <fabriciofelixsilva11@gmail.com>
2024-09-25 13:25:57 -0300

    atv23

---
+++ atv_23.py
@@ -0,0 +1,10 @@
+
+# Recebe um número do usuário
+
+numero = int(input("Digite um número: "))
+
+# Exibe a tabuada de 1 a 10
+
+print(f"Tabuada do {numero}:")
+for i in range(1, 11):
+    print(f"{numero} x {i} = {numero * i}")
